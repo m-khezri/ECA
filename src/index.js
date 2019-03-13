dfc77c9cb907d1c20d5c067832c9dbc097351755
mohammad <khezri@outlook.com>
2019-03-12 22:37:03 -0500

    setup files and folders for authentication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/App/styles/index.scss';
-import App from '../src/App/App';
+import '../src/styles/index.scss';
+import App from '../src/components/App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
